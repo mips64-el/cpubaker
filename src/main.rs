781797c5b1f41397bec1c0b1c28cf77994441dfc
mips64-el <mips64-el@users.noreply.github.com>
2023-10-26 00:11:27 +0200

    1

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use num_cpus;
+use std::{thread, io::{self, Write}};
+
+fn main() {
+    let num_cores = num_cpus::get();
+    let mut handles = vec![];
+
+    for _ in 0..num_cores {
+        handles.push(thread::spawn(move || { 
+            loop {}
+        }));
+    }
+
+    print!("CPU baker already running, if you wish to stop press enter...");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut String::new()).unwrap();
+
+}
